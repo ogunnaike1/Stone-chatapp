a949ceea2ca2abe9d6d36215cc2360d24e0575f9
ogunnaike1 <ogunnaikeusman17@gmail.com>
2026-02-01 23:33:42 +0100

    commit

--- backend/src/Socket/index.js
+++ backend/src/Socket/index.js
@@ -16,7 +16,7 @@ module.exports = (server) => {
       if (!userId) return;
 
       await User.findByIdAndUpdate(userId, { socketId: socket.id, avatar }, { new: true });
-      console.log(`✅ User ${userId} registered with socket ${socket.id}`);
+   
     });
 
     // ---------------- SEND MESSAGE ----------------
