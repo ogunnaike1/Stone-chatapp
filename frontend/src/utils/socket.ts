8ee6703b857d96084b0f3312536a9f4a1bf82c6f
ogunnaike1 <ogunnaikeusman17@gmail.com>
2026-03-20 23:13:28 +0100

    commi

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,8 +1,10 @@
-
-
 import { io } from "socket.io-client";
 
 export const socket = io(import.meta.env.VITE_API_URL || "http://localhost:5002", {
   autoConnect: true,
-  transports: ["websocket", "polling"],
+  transports: ["polling", "websocket"],
+  reconnection: true,
+  reconnectionAttempts: 10,
+  reconnectionDelay: 2000,
+  timeout: 20000,
 });
