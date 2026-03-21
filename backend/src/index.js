5a83220c476bd04e492ab86f0f3c86f77083ab45
ogunnaike1 <ogunnaikeusman17@gmail.com>
2026-03-21 17:45:57 +0100

    added ping bot

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,9 +25,7 @@ app.use("/user",     require("./Route/UserRoute"));
 app.use("/messages", require("./Route/MessageRoute"));
 app.use("/admin",    require("./Route/AdminRoute"));
 
-const port = process.env.PORT || 5002;  // ← fixed
-
-app.get("/ping", (req, res) => res.send("pong"));
+app.get("/ping", (req, res) => res.send("pong"));  
 
 app.get("/", (req, res) => {
     res.send('connected')
